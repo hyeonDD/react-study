3f24958904bae15140b676d5776c69cee6e2e625
hyeon <selwha1@naver.com>
2022-12-28 18:32:33 +0900

    5장 Dom이름짓기 중간커밋

---
+++ react_handling_technology_book/05/naming-dom/src/App.js
@@ -0,0 +1,26 @@
+/* import React, { Component } from 'react';
+import ValidationSample from './ValidationSample';
+
+class App extends Component {
+  render() {
+    return <ValidationSample />;
+  }
+}
+
+export default App;
+ */
+
+import React, { Component } from 'react';
+import ScrollBox from './ScrollBox';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <ScrollBox />
+      </div>
+    );
+  }
+}
+
+export default App;
