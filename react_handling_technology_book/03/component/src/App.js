0f115f46e9339e23db8e7001d56885c31463c8a9
hyeon <selwha1@naver.com>
2022-12-28 16:16:48 +0900

    3장 컴포넌트

---
+++ react_handling_technology_book/03/component/src/App.js
@@ -0,0 +1,25 @@
+/* import MyComponent from './MyComponent';
+
+const App = () => {
+  return (
+    <MyComponent name="React" favoriteNumber={1234}>
+      리액트
+    </MyComponent>
+  );
+}; */
+
+/* import Counter from './Counter';
+
+const App = () => {
+  return <Counter />;
+};
+
+export default App; */
+
+import Say from './Say';
+
+const App = () => {
+  return <Say />;
+};
+
+export default App;
