0c7a5b665c40599d53a8f33f5da0316f20ec5426
hyeon <selwha1@naver.com>
2022-12-30 17:06:36 +0900

    8장 Hooks

---
+++ react_handling_technology_book/08/hooks/src/App.js
@@ -0,0 +1,63 @@
+/* import Counter from './Counter';
+
+const App = () => {
+  return <Counter />;
+}; */
+
+/* import React, { useState } from 'react';
+import Info from './Info';
+
+const App = () => {
+  const [visible, setVisible] = useState(false);
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          setVisible(!visible);
+        }}
+      >
+        {visible ? '숨기기' : '보이기'}
+      </button>
+      <hr />
+      {visible && <Info />}
+    </div>
+  );
+};
+
+export default App;
+*/
+
+/* import Counter from './Counter';
+
+const App = () => {
+  return <Counter />;
+};
+
+export default App;
+ */
+
+/* import Info from './Info';
+
+const App = () => {
+  return <Info />;
+};
+
+export default App;
+ */
+
+/* import Average from './Average';
+
+const App = () => {
+  return <Average />;
+};
+
+export default App;
+ */
+
+import Info from './Info';
+
+const App = () => {
+  return <Info />;
+};
+export default App;
