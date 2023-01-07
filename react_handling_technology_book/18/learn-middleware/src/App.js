0fb4625df59f5258b6d203fd50dffb7d4d72fe3e
hyeon <selwha1@naver.com>
2023-01-07 16:41:02 +0900

    18장 리덕스 미들웨어를 통한 비동기 작업 관리

---
+++ react_handling_technology_book/18/learn-middleware/src/App.js
@@ -0,0 +1,24 @@
+import CounterContainer from './containers/CounterContainer';
+
+const App = () => {
+  return (
+    <div>
+      <CounterContainer />
+    </div>
+  );
+};
+
+export default App;
+
+/* import SampleContainer from './containers/SampleContainer';
+
+const App = () => {
+  return (
+    <div>
+      <SampleContainer />
+    </div>
+  );
+};
+
+export default App;
+ */
