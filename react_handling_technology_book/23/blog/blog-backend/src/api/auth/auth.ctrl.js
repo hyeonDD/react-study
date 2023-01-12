ee7617e2e56ea18609c6e98dbb573bdd794da33c
hyeon <selwha1@naver.com>
2023-01-12 18:14:08 +0900

    23장 중간 커밋

--- react_handling_technology_book/23/blog/blog-backend/src/api/auth/auth.ctrl.js
+++ react_handling_technology_book/23/blog/blog-backend/src/api/auth/auth.ctrl.js
@@ -38,16 +38,73 @@ export const register = async (ctx) => {
 
     // 응답할 데이터에서 hashedPassword 필드 제거
     ctx.body = user.serialize();
+
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+      httpOnly: true,
+    });
   } catch (e) {
     ctx.throw(500, e);
   }
 };
+/* 
+POST /api/auth/login
+{
+  username:'hyeon',
+  password:'mypass123'
+}
+*/
 export const login = async (ctx) => {
-  // 로그인
+  const { username, password } = ctx.request.body;
+
+  // username, password가 없으면 에 처
+  if (!username || !password) {
+    ctx.status = 401;
+    return;
+  }
+
+  try {
+    const user = await User.findByUsername(username);
+    // 계정이 존재하지 않으면 에러 처리
+    if (!user) {
+      ctx.status = 401;
+      return;
+    }
+    const valid = await user.checkPassword(password);
+    // 잘못된 비밀번호
+    if (!valid) {
+      ctx.status = 401;
+      return;
+    }
+    ctx.body = user.serialize();
+    const token = user.generateToken();
+    ctx.cookies.set('access_token', token, {
+      maxAge: 1000 * 60 * 60 * 24 * 7, // 7일
+      httpOnly: true,
+    });
+  } catch (e) {
+    ctx.throw(500, e);
+  }
 };
+
+/* 
+  GET /api/auth/check
+*/
 export const check = async (ctx) => {
-  // 로그인 상태 확인
+  const { user } = ctx.state;
+  if (!user) {
+    // 로그인 중 아님
+    ctx.status = 401; // Unauthorized
+    return;
+  }
+  ctx.body = user;
 };
+
+/* 
+  POST /api/auth/logout
+*/
 export const logout = async (ctx) => {
-  // 로그아웃
+  ctx.cookies.set('access_token');
+  ctx.status = 204;
 };
