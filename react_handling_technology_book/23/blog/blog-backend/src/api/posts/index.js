0ed8783d403afe04f33c2676be0e7c6bf4599995
hyeon <selwha1@naver.com>
2023-01-13 15:15:45 +0900

    JWT를 통한 회원 인증 시스템 구현하기

--- react_handling_technology_book/23/blog/blog-backend/src/api/posts/index.js
+++ react_handling_technology_book/23/blog/blog-backend/src/api/posts/index.js
@@ -1,15 +1,16 @@
 import Router from 'koa-router';
 import * as postsCtrl from './posts.ctrl';
+import checkLoggedIN from '../../lib/checkLoggedIn';
 
 const posts = new Router();
 
 posts.get('/', postsCtrl.list);
-posts.post('/', postsCtrl.write);
+posts.post('/', checkLoggedIN, postsCtrl.write);
 
 const post = new Router(); // /api/posts/:id
 post.get('/', postsCtrl.read);
-post.delete('/', postsCtrl.remove);
-post.patch('/', postsCtrl.update);
-posts.use('/:id', postsCtrl.checkObjectId, post.routes());
+post.delete('/', checkLoggedIN, postsCtrl.checkOwnPost, postsCtrl.remove);
+post.patch('/', checkLoggedIN, postsCtrl.checkOwnPost, postsCtrl.update);
+posts.use('/:id', postsCtrl.getPostById, post.routes());
 
 export default posts;
--- react_handling_technology_book/23/blog/blog-backend/src/api/posts/posts.ctrl.js
+++ react_handling_technology_book/23/blog/blog-backend/src/api/posts/posts.ctrl.js
@@ -13,11 +13,23 @@ POST /api/posts
 
 const { ObjectId } = mongoose.Types;
 
-export const checkObjectId = (ctx, next) => {
+export const getPostById = async (ctx, next) => {
   const { id } = ctx.params;
   if (!ObjectId.isValid(id)) {
     ctx.status = 400;
     return;
+  }
+  try {
+    const post = await Post.findById(id);
+    // 포스트가 존재 하지 않을 때
+    if (!post) {
+      ctx.status = 404; // Not Found
+      return;
+    }
+    ctx.state.post = post;
+    return next();
+  } catch (e) {
+    ctx.throw(500, e);
   }
   return next();
 };
@@ -42,7 +54,9 @@ export const write = async (ctx) => {
     title,
     body,
     tags,
+    user: ctx.state.user,
   });
+
   try {
     await post.save();
     ctx.body = post;
@@ -60,37 +74,33 @@ export const list = async (ctx) => {
     return;
   }
 
+  const { tag, username } = ctx.query;
+  // tag, username 값이 유효하면 객체 안에 넣고, 그렇지 않으면 넣지 않음
+  const query = {
+    ...(username ? { 'user.username': username } : {}),
+    ...(tag ? { tags: tag } : {}),
+  };
+
   try {
-    const posts = await Post.find()
+    const posts = await Post.find(query)
       .sort({ _id: -1 })
       .limit(10)
       .skip((page - 1) * 10)
+      .lean()
       .exec();
-    const postCount = await Post.countDocuments().exec();
+    const postCount = await Post.countDocuments(query).exec();
     ctx.set('Last-Page', Math.ceil(postCount / 10));
-    ctx.body = posts
-      .map((post) => post.toJSON())
-      .map((post) => ({
-        ...post,
-        body:
-          post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
-      }));
+    ctx.body = posts.map((post) => ({
+      ...post,
+      body:
+        post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+    }));
   } catch (e) {
     ctx.throw(500, e);
   }
 };
-export const read = async (ctx) => {
-  const { id } = ctx.params;
-  try {
-    const post = await Post.findById(id).exec();
-    if (!post) {
-      ctx.status = 404;
-      return;
-    }
-    ctx.body = post;
-  } catch (e) {
-    ctx.throw(500, e);
-  }
+export const read = (ctx) => {
+  ctx.body = ctx.state.post;
 };
 /* 
   DELETE /api/posts/:id
@@ -141,4 +151,13 @@ export const update = async (ctx) => {
   } catch (e) {
     ctx.throw(500, e);
   }
+};
+
+export const checkOwnPost = (ctx, next) => {
+  const { user, post } = ctx.state;
+  if (post.user._id.toString() !== user._id) {
+    ctx.status = 403;
+    return;
+  }
+  return next();
 };
