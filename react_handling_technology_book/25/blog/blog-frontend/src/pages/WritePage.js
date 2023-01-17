39a57b2ac2f3175e212c5bee5c49cd2c94a94123
hyeon <selwha1@naver.com>
2023-01-17 16:12:48 +0900

    25장 중간커밋

---
+++ react_handling_technology_book/25/blog/blog-frontend/src/pages/WritePage.js
@@ -0,0 +1,16 @@
+import TagBox from '../components/write/TagBox';
+import WriteActionButtons from '../components/write/WriteActionButtons';
+import Responsive from '../components/common/Responsive';
+import EditorContainer from '../containers/write/EditorContainer';
+
+const WritePage = () => {
+  return (
+    <Responsive>
+      <EditorContainer />
+      <TagBox />
+      <WriteActionButtons />
+    </Responsive>
+  );
+};
+
+export default WritePage;
