1afbfae52991f792cf9ccb6d632aee20bf91d1ea
hyeon <selwha1@naver.com>
2023-01-18 21:03:49 +0900

    26장 중간커밋

---
+++ react_handling_technology_book/26/blog/blog-frontend/src/pages/PostListPage.js
@@ -0,0 +1,13 @@
+import HeaderContainer from '../containers/common/HeaderContainer';
+import PostList from '../components/posts/PostList';
+
+const PostListPage = () => {
+  return (
+    <>
+      <HeaderContainer />
+      <PostList />
+    </>
+  );
+};
+
+export default PostListPage;
---
+++ react_handling_technology_book/26/blog/blog-frontend/src/pages/WritePage.js
@@ -0,0 +1,17 @@
+// import TagBox from '../components/write/TagBox';
+import Responsive from '../components/common/Responsive';
+import EditorContainer from '../containers/write/EditorContainer';
+import TagBoxContainer from '../containers/write/TagBoxContainer';
+import WriteActionButtonsContainer from '../containers/write/WriteActionButtonscontainer';
+
+const WritePage = () => {
+  return (
+    <Responsive>
+      <EditorContainer />
+      <TagBoxContainer />
+      <WriteActionButtonsContainer />
+    </Responsive>
+  );
+};
+
+export default WritePage;
